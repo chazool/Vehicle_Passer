a3cbe955d03a181994ec958e292b6c8e33312826
chazool <chazoolkaweesha@gmail.com>
2021-05-15 17:40:23 +0530

    add new Zuul Cloud Gateway

---
+++ cloudservice-gateway/src/test/java/com/chazool/highwayvehiclepasser/cloudservicegateway/CloudserviceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chazool.highwayvehiclepasser.cloudservicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudserviceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
